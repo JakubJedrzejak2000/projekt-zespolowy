36bd812a9a5e7aaa1eb4ff92a057224d196fd8cf
JakubJedrzejak2000 <JakubJedrzejak2000@users.noreply.github.com>
2022-04-22 23:34:34 +0200

    Terraform v1.0 and README update

--- README.md
+++ README.md
@@ -39,3 +39,89 @@ Uzytkownik ma możliwość zaproponowania własnego cytatu, który trafi do bazy
 
 ## Architektura
 * Klient - Serwer
+
+
+## Uruchomienie aplikacji lokalnie
+Aby ułatwić uruchamianie aplikacji lokalnie na komputerze, stworzony został plik docker-compose. Wystarczy w katalogu z tym plikiem wpisać
+```
+docker-compose up
+```
+Plik docker-compose zawiera w sobie bazę danych, backend oraz Prometheusa wraz z Grafaną. W celu uruchomienia frontendu należy wpisać najpierw komendę
+```
+npm i
+```
+Zainstaluje ona wszelkie potrzebne do działania biblioteki. Komenda
+```
+npm run watch
+```
+Uruchomi aplikację
+
+## Uruchomienie aplikacji w chmurze Azure'a
+
+Aplikację można uruchomić w chmurze Azure'a. Aby to uzyskać, użyty został zarówno Terraform jak i Azure CLI. Terraform służ do zuploadowania frontendu oraz stworzenia grupy zasobów.
+Zdeployowanie aplikacji przy pomocy Terraforma jest proste. Trzeba najpierw wpisać komendę
+```
+terraform init
+```
+Jeśli wszystko zakończyło się sukcesem, następną komendą będzie
+```
+terraform apply
+```
+Terraform zapyta użytkownika o dane, które ten musi podać. Następnie wystarczy jeszcze wpisać "yes" i aplikacja została zdeployowana.
+
+Zdeployowanie bazy danych oraz frontendu odbywa się za pomocą komend azure cli.
+Najpierw trzeba określić kilka zmiennych, które będą się powtarzac
+```
+export resource_group={nazwa grupy zasobow, którą podano wcześniej}
+export user_password={haslo uzytkownika}
+export user_login={login uzytkownika}
+export plan_name={nazwa planu}
+export webapp_name={nazwa aplikacji}
+```
+Po stworzeniu kilku zmiennych, należy rozpocząć od stworzenia bazy danych postgreSQL
+```
+az postgres flexible-server create \
+--admin-password $user_password \
+--admin-user $user_login \
+--location eastus \
+--name service-db \
+--resource-group $resource_group \
+--storage-size 32 \
+--public-access all \
+--sku-name Standard_B1ms \
+--tier Burstable \
+--public-access 0.0.0.0
+```
+Po udanym zdeployowaniu Bazy Danych, konsola wyświetli kilka przydatnych danych. Warto zapisać sobie nazwę bazy danych oraz hosta.
+```
+export databse_name={wartosc databaseName}
+export host{wartosc host}
+```
+
+Następnym krokiem jest stworzenie Serivce Planu dla Web appa.
+```
+az appservice plan create \
+--name $plan_name \
+--resource-group $resource_group \
+--location eastus \
+--sku P1V2 \
+--is-linux
+```
+Po udanym zdeployowaniu trzeba jeszcze stworzyć samą aplikację.
+```
+az webapp create \
+--name $webapp_name \
+--plan $plan_name \
+--resource-group $resource_group \
+--deployment-container-image-name oskarskalski/quote-service:latest
+```
+Aby aplikacja działa w pełni sprawnie należy dorzucić jeszcze zmienne środowiskowe.
+```
+az webapp config appsettings set \
+--name $webapp_name \
+--resource-group $resource_group \
+--settings ENVIRONMENT=$host \
+DATABASE_NAME=$database_name \
+DATABASE_USERNAME=$user_login \
+DATABASE_PASSWORD=$user_password
+```
--- main.tf
+++ main.tf
@@ -1,152 +1,36 @@
 provider "azurerm" {
-  features {
-
-  }
+  features {}
 }
 
-resource "azurerm_resource_group" "example" {
-  name     = "api-rg-pro"
+
+variable "resourcegroupname" {
+  description = "Resource group name"
+}
+
+resource "azurerm_resource_group" "resource_group" {
+  name     = var.resourcegroupname
   location = "West Europe"
 }
 
-variable "login" {
-  description = "Administrator login for postgresql database"
-}
+resource "azurerm_app_service_plan" "service_plan" {
+  name                = "service_plan_frontend"
+  location            = azurerm_resource_group.resource_group.location
+  resource_group_name = azurerm_resource_group.resource_group.name
 
-variable "password" {
-  description = "Aministrator password for postgresql database"
-
-}
-
-resource "azurerm_postgresql_server" "example" {
-  name                = "moj-testowy-serwer-na-projekt-jakubj"
-  location            = azurerm_resource_group.example.location
-  resource_group_name = azurerm_resource_group.example.name
-
-  sku_name = "B_Gen5_2"
-
-  storage_mb                   = 5120
-  backup_retention_days        = 7
-  geo_redundant_backup_enabled = false
-  auto_grow_enabled            = true
-
-  administrator_login          = var.login
-  administrator_login_password = var.password
-  version                      = "11"
-  ssl_enforcement_enabled      = true
-}
-
-resource "azurerm_postgresql_firewall_rule" "example" {
-  name                = "office"
-  resource_group_name = azurerm_resource_group.example.name
-  server_name         = azurerm_postgresql_server.example.name
-  start_ip_address    = "0.0.0.0"
-  end_ip_address      = "255.255.255.255"
-}
-
-resource "azurerm_postgresql_database" "example" {
-  name                = "exampledb"
-  resource_group_name = azurerm_resource_group.example.name
-  server_name         = azurerm_postgresql_server.example.name
-  charset             = "UTF8"
-  collation           = "English_United States.1252"
-
-}
-
-# Create the Linux App Service Plan
-resource "azurerm_app_service_plan" "appserviceplan" {
-  name                = "webapp-asp-frontend-quote-generator-jakubj"
-  location            = azurerm_resource_group.example.location
-  resource_group_name = azurerm_resource_group.example.name
   sku {
-    tier = "Free"
-    size = "F1"
+    tier = "Standard"
+    size = "S1"
   }
 }
-# Create the web app, pass in the App Service Plan ID, and deploy code from a public GitHub repo
-resource "azurerm_app_service" "webapp" {
-  name                = "webapp-frontend-quote-generator-jakubj"
-  location            = azurerm_resource_group.example.location
-  resource_group_name = azurerm_resource_group.example.name
-  app_service_plan_id = azurerm_app_service_plan.appserviceplan.id
+
+resource "azurerm_app_service" "frontend_app_service" {
+  name                = "frontend_app_service"
+  location            = azurerm_resource_group.resource_group.location
+  resource_group_name = azurerm_resource_group.resource_group.name
+  app_service_plan_id = azurerm_app_service_plan.service_plan.id
+
   source_control {
-    repo_url           = "https://github.com/JakubJedrzejak2000/projekt-zespolowy"
-    branch             = "master"
-    manual_integration = true
-    use_mercurial      = false
+    repo_url = "https://github.com/JakubJedrzejak2000/projekt-zespolowy"
+    branch   = "frontend"
   }
-}
-
-locals {
-  env_variables = {
-    ENVIRONMENT       = "${azurerm_postgresql_database.example.server_name}.postgres.database.azure.com"
-    DATABASE_NAME     = "postgres"
-    DATABASE_USERNAME = "${azurerm_postgresql_server.example.administrator_login}@${azurerm_postgresql_database.example.server_name}"
-    DATABASE_PASSWORd = var.password
-  }
-}
-
-output "values" {
-  value = azurerm_app_service.webapp
-}
-
-output "values2" {
-  value = azurerm_app_service_plan.appserviceplan
-}
-
-output "databse" {
-  value = azurerm_postgresql_database.example
-}
-
-output "dbserver" {
-  sensitive = true
-  value     = azurerm_postgresql_server.example
-}
-
-output "locals" {
-  value = local.env_variables
-}
-
-
-resource "azurerm_container_group" "example" {
-  name                = "example-containet-jakubj"
-  location            = azurerm_resource_group.example.location
-  resource_group_name = azurerm_resource_group.example.name
-  ip_address_type     = "Public"
-  # dns_name_label      = "aci-label"
-  os_type             = "Linux"
-
-  container {
-    name   = "quote-generator"
-    image  = "oskarskalski/quote-service:latest"
-    cpu    = "0.5"
-    memory = "1.5"
-
-    ports {
-      port     = 443
-      protocol = "TCP"
-    }
-  }
-}
-
-output "docker" {
-  value     = azurerm_container_group.example.container
-  sensitive = true
-}
-
-resource "azurerm_app_service" "webappdockerfrontend" {
-  depends_on = [
-    azurerm_app_service.webapp
-  ]
-  name                = "docker-backend-quote-generator-jakubj"
-  location            = azurerm_resource_group.example.location
-  resource_group_name = azurerm_resource_group.example.name
-  app_service_plan_id = azurerm_app_service_plan.appserviceplan.id
-  site_config {
-    scm_type  = "VSTSRM"
-    always_on = "true"
-
-    linux_fx_version = "DOCKER|20.101.165.47"
-  }
-  app_settings = local.env_variables
 }
